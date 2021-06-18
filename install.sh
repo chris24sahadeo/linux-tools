a441f8b0c4c3e0b9b4d90cf370ec094abea6d899
Christopher Sahadeo <chris@virtanatech.com>
2021-06-18 11:44:43 -0400

    Added .tmux.conf

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ EOF
 # Vim looks for .vimrc in the $HOME directory.
 # A simlink is a nice way for .vim to find the file, while we still track in it git.
 ln -s $THIS_SCRIPT_DIR/.vimrc ~/.vimrc
+
+# Simlink for .tmux.conf.
+ls -s $THIS_SCRIPT_DIR/.tmux.conf ~/.tmux.conf
