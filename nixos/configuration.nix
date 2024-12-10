cc9745edc96a148a38cd8b406bfe8405b33e335e
Christopher Sahadeo <chris@virtanatech.com>
2024-12-09 20:16:50 -0400

    added mod config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -39,6 +39,8 @@
     delta
     jq
     neovim
+    lshw
+    zoom-us
   ];
 
   environment.shellAliases = {
@@ -152,6 +154,8 @@
 
   # Install firefox.
   programs.firefox.enable = true;
+
+  programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
