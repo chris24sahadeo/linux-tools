b88f4d2d404aacd09e0ec320576b95113250e0f2
Chris Sahadeo <chris@virtanatech.com>
2024-12-12 11:44:21 -0400

    nuc sync

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,7 +11,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    nvtop
+#    nvtop
     google-chrome
     vscode
     warp-terminal
@@ -44,42 +44,47 @@
     zoom-us
   ];
 
+ programs.git.config.user = {
+  name = "Chris Sahadeo";
+  email = "chris@virtanatech.com";
+};
+
   # START: Nvidia...
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
+#   services.xserver.videoDrivers = ["nvidia"];
+# 
+#   hardware.nvidia = {
+# 
+#     # Modesetting is required.
+#     modesetting.enable = true;
+# 
+#     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+#     # Enable this if you have graphical corruption issues or application crashes after waking
+#     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+#     # of just the bare essentials.
+#     powerManagement.enable = false;
+# 
+#     # Fine-grained power management. Turns off GPU when not in use.
+#     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+#     powerManagement.finegrained = false;
+# 
+#     # Use the NVidia open source kernel module (not to be confused with the
+#     # independent third-party "nouveau" open source driver).
+#     # Support is limited to the Turing and later architectures. Full list of 
+#     # supported GPUs is at: 
+#     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+#     # Only available from driver 515.43.04+
+#     # Currently alpha-quality/buggy, so false is currently the recommended setting.
+#     open = false;
+# 
+#     # Enable the Nvidia settings menu,
+# 	# accessible via `nvidia-settings`.
+#     nvidiaSettings = true;
+# 
+#     # Optionally, you may need to select the appropriate driver version for your specific GPU.
+#     package = config.boot.kernelPackages.nvidiaPackages.stable;
+#   };
+# 
 #  hardware.opengl.mesaPackage = pkgs.mesa;
 
   # END: Nvidia...
@@ -113,7 +118,7 @@
   
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix.luigi
+      ./hardware-configuration.nix.nuc
     ];
 
   # Bootloader.
@@ -123,7 +128,7 @@
 #    boot.initrd.luks.devices."luks-b8b53034-d87d-44f4-a05e-81181219b9d7".device = "/dev/disk/by-uuid/b8b53034-d87d-44f4-a05e-81181219b9d7"; # the-beast
 
 #    boot.initrd.luks.devices."luks-636e564f-ece5-4bb0-92ff-7992945f96eb".device = "/dev/disk/by-uuid/636e564f-ece5-4bb0-92ff-7992945f96eb"; # nuc
-    boot.initrd.luks.devices."luks-ebda0408-492f-4b11-baae-d9667054fe66".device = "/dev/disk/by-uuid/ebda0408-492f-4b11-baae-d9667054fe66"; # luigi
+#    boot.initrd.luks.devices."luks-ebda0408-492f-4b11-baae-d9667054fe66".device = "/dev/disk/by-uuid/ebda0408-492f-4b11-baae-d9667054fe66"; # luigi
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
