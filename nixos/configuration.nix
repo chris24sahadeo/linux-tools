081b6ca3d260f596c0eaac736cab14d6a7f6f3c7
Chris Sahadeo <chris@virtanatech.com>
2024-12-13 15:56:41 -0400

    synced luigi

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -64,41 +64,40 @@ services.tailscale.enable = true;
 # TODO: move to a nvidia config file.
   # START: Nvidia...
   # Load nvidia driver for Xorg and Wayland
-#   services.xserver.videoDrivers = ["nvidia"];
-# 
-#   hardware.nvidia = {
-# 
-#     # Modesetting is required.
-#     modesetting.enable = true;
-# 
-#     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-#     # Enable this if you have graphical corruption issues or application crashes after waking
-#     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-#     # of just the bare essentials.
-#     powerManagement.enable = false;
-# 
-#     # Fine-grained power management. Turns off GPU when not in use.
-#     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-#     powerManagement.finegrained = false;
-# 
-#     # Use the NVidia open source kernel module (not to be confused with the
-#     # independent third-party "nouveau" open source driver).
-#     # Support is limited to the Turing and later architectures. Full list of 
-#     # supported GPUs is at: 
-#     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-#     # Only available from driver 515.43.04+
-#     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-#     open = false;
-# 
-#     # Enable the Nvidia settings menu,
-# 	# accessible via `nvidia-settings`.
-#     nvidiaSettings = true;
-# 
-#     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-#     package = config.boot.kernelPackages.nvidiaPackages.stable;
-#   };
-# 
-#  hardware.opengl.mesaPackage = pkgs.mesa;
+   services.xserver.videoDrivers = ["nvidia"];
+ 
+   hardware.nvidia = {
+ 
+     # Modesetting is required.
+     modesetting.enable = true;
+ 
+     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+     # Enable this if you have graphical corruption issues or application crashes after waking
+     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+     # of just the bare essentials.
+     powerManagement.enable = false;
+ 
+     # Fine-grained power management. Turns off GPU when not in use.
+     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+     powerManagement.finegrained = false;
+ 
+     # Use the NVidia open source kernel module (not to be confused with the
+     # independent third-party "nouveau" open source driver).
+     # Support is limited to the Turing and later architectures. Full list of 
+     # supported GPUs is at: 
+     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+     # Only available from driver 515.43.04+
+     # Currently alpha-quality/buggy, so false is currently the recommended setting.
+     open = false;
+ 
+     # Enable the Nvidia settings menu,
+ 	# accessible via `nvidia-settings`.
+     nvidiaSettings = true;
+ 
+     # Optionally, you may need to select the appropriate driver version for your specific GPU.
+     package = config.boot.kernelPackages.nvidiaPackages.stable;
+   };
+ 
 
   # END: Nvidia...
   
@@ -152,7 +151,6 @@ services.tailscale.enable = true;
 
 # TODO: remove once other PCs are reformatted to remove disk level encryption
 #    boot.initrd.luks.devices."luks-b8b53034-d87d-44f4-a05e-81181219b9d7".device = "/dev/disk/by-uuid/b8b53034-d87d-44f4-a05e-81181219b9d7"; # the-beast
-#    boot.initrd.luks.devices."luks-ebda0408-492f-4b11-baae-d9667054fe66".device = "/dev/disk/by-uuid/ebda0408-492f-4b11-baae-d9667054fe66"; # luigi
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
